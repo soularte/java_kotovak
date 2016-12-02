a64533dc7bf7f04fdf78d18f7e16fa007c9b22c3
soularte <freziachild@gmail.com>
2016-12-02 22:18:51 +0400

    Few functions are added

--- sandbox/src/main/java/TestProgram.java
+++ sandbox/src/main/java/TestProgram.java
@@ -1,10 +1,28 @@
-
-
 public class TestProgram {
 
-    public static void main(String[] args) {
-        System.out.println("Hello!");
+  public static void main(String[] args) {
+    hello("world");
+    hello("user Alex");
 
-    }
+    double l = 5;
+            System.out.println("Площать квадрата со стороной " + l + " = " + area(l));
 
+    double a = 4;
+    double b = 6;
+
+    System.out.println("Площать прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+  }
+
+  public static void hello(String smb) {
+    System.out.println("Hello, " + smb + "!");
+
+  }
+
+  public static double area(double len){
+    return len * len;
+  }
+
+  public static double area(double a, double b){
+    return a * b;
+  }
 }
--- sandbox/src/main/java/kkotova/sandbox/myFirstProgram.java
+++ sandbox/src/main/java/kkotova/sandbox/myFirstProgram.java
@@ -3,7 +3,12 @@ package kkotova.sandbox;
 public class MyFirstProgram {
 
     public static void main(String[] args) {
-        System.out.println("Hello!");
+        String smb = "world";
+        System.out.println("Hello," + smb);
+
+        double l = 8.1;
+        double s = l * l;
+        System.out.println("Площадь квадрата со сторной " + l + " = " + s);
 
     }
 
