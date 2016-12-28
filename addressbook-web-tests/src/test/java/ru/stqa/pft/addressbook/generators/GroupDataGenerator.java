383ca7c86d6072ce1871525f86557f11073065a5
soularte <freziachild@gmail.com>
2016-12-28 17:31:54 +0400

    Генерация данных для групп, xml

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/GroupDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/GroupDataGenerator.java
@@ -3,12 +3,14 @@ package ru.stqa.pft.addressbook.generators;
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.ParameterException;
+import com.thoughtworks.xstream.XStream;
 import ru.stqa.pft.addressbook.model.GroupData;
 
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Writer;
+import java.security.acl.Group;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,9 +22,18 @@ public class GroupDataGenerator {
   @Parameter(names = "-f", description = "Target file")
   public String file;
 
+  @Parameter(names = "-d", description = "Data format")
+  public String format;
+
   private void run() throws IOException {
     List<GroupData> groups = generateGroups(count);
-    save(groups, new File(file));
+    if (format.equals("csv") ) {
+      saveAsCsv(groups, new File(file));
+    } else if (format.equals ("xml")){
+      saveAsXml(groups, new File(file));
+    } else {
+      System.out.println("Unrecognized format" + format);
+    }
   }
 
   public static void main(String[] args) throws IOException {
@@ -37,13 +48,23 @@ public class GroupDataGenerator {
     generator.run();
   }
 
-  private void save(List<GroupData> groups, File file) throws IOException {
+  private void saveAsCsv(List<GroupData> groups, File file) throws IOException {
     Writer writer = new FileWriter(file);
     for (GroupData group : groups) {
       writer.write(String.format("%s;%s;%s\n", group.getGroupName(), group.getGroupHeader(), group.getGroupFooter()));
     }
     writer.close();
   }
+
+  private void saveAsXml(List<GroupData> groups, File file) throws IOException {
+    XStream xstream = new XStream();
+    xstream.processAnnotations(GroupData.class);
+    String xml = xstream.toXML(groups);
+    Writer writer = new FileWriter(file);
+    writer.write(xml);
+    writer.close();
+  }
+
 
   private List<GroupData> generateGroups(int count) {
     List<GroupData> groups = new ArrayList<GroupData>();
