32b91dfc3f86568c9d855a4737794c4f1d4f4c03
soularte <freziachild@gmail.com>
2017-01-25 13:13:44 +0400

    Changes

---
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/model/UserData.java
@@ -0,0 +1,53 @@
+package ru.stqa.pft.mantis.model;
+
+import com.google.gson.annotations.Expose;
+import org.hibernate.annotations.Type;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "mantis_user_table")
+public class UserData {
+  @Id
+  @Column(name = "id")
+  private int id = Integer.MAX_VALUE;
+
+  @Expose
+  @Column(name = "username")
+  private String userName;
+
+  @Expose
+  @Column(name = "email")
+  private String email;
+
+  public int getId() {
+    return id;
+  }
+
+  public String getUserName() {
+    return userName;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public UserData withId(int id) {
+    this.id = id;
+    return this;
+  }
+
+  public UserData withUserName(String userName) {
+    this.userName = userName;
+    return this;
+  }
+
+  public UserData withEmail(String email) {
+    this.email = email;
+    return this;
+  }
+}
---
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/ChangePasswordTest.java
@@ -0,0 +1,54 @@
+package ru.stqa.pft.mantis.tests;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import ru.lanwen.verbalregex.VerbalExpression;
+import ru.stqa.pft.mantis.appmanager.HttpSession;
+import ru.stqa.pft.mantis.model.MailMessage;
+import ru.stqa.pft.mantis.model.Data;
+import ru.stqa.pft.mantis.model.Users;
+
+import javax.mail.MessagingException;
+import java.io.IOException;
+import java.util.List;
+
+import static org.testng.Assert.assertTrue;
+
+public class ChangePasswordTest extends TestBase{
+  @BeforeMethod
+  public void startMailServer(){
+    app.mail().start();
+  }
+
+  @Test
+  public void testChangePassword() throws IOException, MessagingException {
+    Users before=app.db().users();
+    UserData modifiedUser=before.iterator().next();
+    String userName=modifiedUser.getUserName();
+    String newPassword="root1";
+    String email = modifiedUser.getEmail();
+    app.session();
+    app.goTo().manageUsersPage();
+    app.users().manageUser(userName);
+    app.users().startResetPassword();
+    List<MailMessage> mailMessages = app.mail().waitForMail(1, 10000);
+    String confirmationLink = findConfirmationLink(mailMessages, email);
+    app.registration().finish(confirmationLink,newPassword);
+
+    HttpSession session=app.newSession();
+    assertTrue(session.login(userName,newPassword));
+  }
+
+  private String findConfirmationLink(List<MailMessage> mailMessages, String email) {
+    MailMessage mailMessage = mailMessages.stream().filter((m) -> m.to.equals(email)).findFirst().get();
+    VerbalExpression regex=VerbalExpression.regex().find("http://").nonSpace().oneOrMore().build();
+    return regex.getText(mailMessage.text);
+  }
+
+  @AfterMethod(alwaysRun = true)
+  public void stopMailServer(){
+    app.mail().stop();
+  }
+
+}
