203de934ea6afbefbcaec8d59d87706e2daf99f0
soularte <freziachild@gmail.com>
2019-06-04 23:15:38 +0300

    Task 3 - Login

--- java_example/src/test/java/LiteCardLogin.java
+++ java_example/src/test/java/LiteCardLogin.java
@@ -2,29 +2,30 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+import java.util.concurrent.TimeUnit;
 
-public class FirstTest {
+public class LiteCardLogin {
 
     private WebDriver driver;
     private WebDriverWait wait;
+    private String adress = "http://localhost/litecart/en/";
 
     @Before
     public void start() {
         driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         wait = new WebDriverWait(driver, 10);
     }
 
     @Test
     public void firstTest() {
-        driver.get("http://www.google.com/");
-        driver.findElement(By.name("q")).sendKeys("webdriver");
-        driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
-        wait.until(titleIs("webdriver - Поиск в Google"));
+        driver.get(adress);
+        driver.findElement(By.cssSelector("input[type='text']")).sendKeys("admin");
+        driver.findElement(By.cssSelector("input[type='password']")).sendKeys("admin");
+        driver.findElement(By.cssSelector("button[name='login']")).click();
     }
 
     @After
