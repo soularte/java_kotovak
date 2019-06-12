dbbfd038bfb89a3ca306a0e2fce9d5a3984f486a
soularte <freziachild@gmail.com>
2019-06-12 21:41:28 +0300

    Task 7

--- java_example/src/test/java/TestBase.java
+++ java_example/src/test/java/TestBase.java
@@ -1,17 +1,30 @@
 import org.junit.After;
 import org.junit.Before;
+import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import java.util.concurrent.TimeUnit;
 
 public class TestBase {
 
     public static WebDriver driver;
     public static WebDriverWait wait;
 
+    public boolean isElementPresent(By locator) {
+        try {
+            wait.until((WebDriver d) -> d.findElement(locator));
+            return true;
+        } catch (TimeoutException ex) {
+            return false;
+        }
+    }
+
     @Before
     public void start() {
         driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         wait = new WebDriverWait(driver, 10);
     }
 
