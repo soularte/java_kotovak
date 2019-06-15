22761557e18cf7df05aebb7c9807a3c1ad1ded35
soularte <freziachild@gmail.com>
2019-06-15 12:28:30 +0300

    Task 8 update

--- java_example/src/test/java/LiteCardStikers.java
+++ java_example/src/test/java/LiteCardStikers.java
@@ -1,15 +1,8 @@
 import junit.framework.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebElement;
-
-import javax.lang.model.element.Element;
-import javax.lang.model.util.Elements;
-import java.lang.reflect.Array;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.ListIterator;
 
 public class LiteCardStikers extends TestBase {
 
@@ -20,7 +13,7 @@ public class LiteCardStikers extends TestBase {
     public void stikersTest() {
         driver.get(adress);
         List<WebElement> items = driver.findElements(By.xpath("//div[@class='image-wrapper']"));
-        for ( WebElement item: items ) {
+        for (WebElement item : items) {
             int numberOfStikers = item.findElements(By.xpath("./div[starts-with(@class, 'sticker')]")).size();
             Assert.assertEquals(1, numberOfStikers);
         }
