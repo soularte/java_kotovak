a617818448c2beb6aa84b3c80ac68993142cc2c6
soularte <freziachild@gmail.com>
2019-06-15 12:27:16 +0300

    Task 8

--- java_example/src/test/java/LiteCardStikers.java
+++ java_example/src/test/java/LiteCardStikers.java
@@ -1,30 +1,28 @@
+import junit.framework.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.StaleElementReferenceException;
+import org.openqa.selenium.WebElement;
+
+import javax.lang.model.element.Element;
+import javax.lang.model.util.Elements;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
 
 public class LiteCardStikers extends TestBase {
 
-    private String adress = "http://localhost/litecart/admin/";
-    private String login = "admin";
+    private String adress = "http://localhost/litecart/en/";
+    private Object items;
 
     @Test
-    public void adminTest() {
-        login();
-        int numberOfLinks = driver.findElements(By.xpath("//*[@id='box-apps-menu']/li")).size();
-            for (int i = 1; i <= numberOfLinks; i++) {
-                driver.findElement(By.xpath("//*[@id='box-apps-menu']/li[" + i + "]/a")).click();
-                int numberOfInnerLinks = driver.findElements(By.xpath("//*[@id='box-apps-menu']/li[" + i + "]/ul/li/a")).size();
-                    for (int a = 1; a <= numberOfInnerLinks; a++) {
-                        driver.findElement(By.xpath("//*[@id='box-apps-menu']/li[" + i + "]/ul/li[" + a + "]/a")).click();
-                        isElementPresent(By.xpath("//*[@id='content']/h1"));
-                    }
-                }
-    }
-
-    public void login() {
+    public void stikersTest() {
         driver.get(adress);
-        driver.findElement(By.cssSelector("input[type='text']")).sendKeys(login);
-        driver.findElement(By.cssSelector("input[type='password']")).sendKeys(login);
-        driver.findElement(By.cssSelector("button[name='login']")).click();
+        List<WebElement> items = driver.findElements(By.xpath("//div[@class='image-wrapper']"));
+        for ( WebElement item: items ) {
+            int numberOfStikers = item.findElements(By.xpath("./div[starts-with(@class, 'sticker')]")).size();
+            Assert.assertEquals(1, numberOfStikers);
+        }
     }
 }
